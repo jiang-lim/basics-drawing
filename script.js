4ed42be644b5a8371f89c47f93734f72e2f5a56f
jiang-lim <writetojianglim@gmail.com>
2021-06-10 01:16:58 +0800

    attempt of exercises in 9.2

--- script.js
+++ script.js
@@ -1,4 +1,55 @@
+// square
+
+// var main = function (input) {
+//   var myOutputValue = "";
+//   var rowCounter = 0;
+//   while (rowCounter < input) {
+//     var columnCounter = 0;
+//     while (columnCounter < input) {
+//       myOutputValue += "🐱‍🐉";
+//       columnCounter += 1;
+//     }
+//     myOutputValue += `</br>`;
+//     rowCounter += 1;
+//   }
+//   return myOutputValue;
+// };
+
+// triangle
+
+// var main = function (input) {
+//   var myOutputValue = "";
+//   var rowCounter = 0;
+//   while (rowCounter < input) {
+//     var columnCounter = 0;
+//     while (columnCounter <= rowCounter) {
+//       myOutputValue += "🐱‍🐉";
+//       columnCounter += 1;
+//     }
+//     myOutputValue += `</br>`;
+//     rowCounter += 1;
+//   }
+//   return myOutputValue;
+// };
+
+// outline square
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = "";
+  var rowCounter = 0;
+  while (rowCounter < input) {
+    var columnCounter = 0;
+    while (columnCounter < input) {
+      if (columnCounter == 0 || columnCounter == input - 1 || rowCounter == 0 || rowCounter == input - 1) {
+        myOutputValue += "🐱‍🐉";
+        columnCounter += 1;
+      } else {
+        myOutputValue += "🚀";
+        columnCounter += 1;
+      }
+    }
+    myOutputValue += `</br>`;
+    rowCounter += 1;
+  }
   return myOutputValue;
 };
